ada994728b765d57e011d660f8cd7f9979917796
butibuti <butikouki@gmail.com>
2022-04-30 12:35:35 +0900

    init commit

---
+++ Header/GameComponentHeader.h
@@ -0,0 +1,9 @@
+#ifndef GameComponentHeader
+#define GameComponentHeader
+
+#pragma once
+#include"GameObjects/GameComponent.h"
+#include "Header\Common\CerealUtil.h"
+
+
+#endif // !GameComponentHeader
