728de09bac94dba0a42cdc2be13b7c36f42504cb
butibuti <butikouki@gmail.com>
2022-09-09 14:38:00 +0900

    worldSpeedの修正

--- Header/Device/GameDevice.h
+++ Header/Device/GameDevice.h
@@ -1,21 +1,26 @@
+#ifndef _GAMEDEVICE_H
+#define _GAMEDEVICE_H
+
 #pragma once
 
 #include"../Device/Input.h"
 namespace ButiEngine {
-
-	class GameDevice
-	{
-	public:
-		~GameDevice();
-		static void Initialize();
-		static float WorldSpeed;
-		static std::unique_ptr<Input>& GetInput();
-		static void SetIsEnd(const bool arg_isEnd);
-		static bool GetIsEnd();
-		static void SetReturnCode(const std::int32_t arg_returnCode);
-		static std::int32_t GetReturnCode();
-	private:
-		GameDevice();
-		static std::unique_ptr<Input> input;
-	};
+class GameDevice
+{
+public:
+	~GameDevice();
+	static void Initialize();
+	static float GetWorldSpeed();
+	static void SetWorldSpeed(const float arg_speed);
+	static std::unique_ptr<Input>& GetInput();
+	static void SetIsEnd(const bool arg_isEnd);
+	static bool GetIsEnd();
+	static void SetReturnCode(const std::int32_t arg_returnCode);
+	static std::int32_t GetReturnCode();
+private:
+	GameDevice();
+	static std::unique_ptr<Input> input;
+};
 }
+
+#endif // !_GAMEDEVICE_H
