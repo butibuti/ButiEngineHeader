6646642deed0045247685d1d81cb897b89720f7a
butibuti <butikouki@gmail.com>
2022-12-14 13:54:22 +0900

    GameObject複数選択

--- Header/GameParts/GameObjectManager.h
+++ Header/GameParts/GameObjectManager.h
@@ -46,16 +46,18 @@ public:
 	}
 	void CreateViewGameObjectVector(const std::string& arg_serchStr, const std::vector<ButiEngine::GameObjectTag>& arg_vec_serchTags);
 	List<std::string> GetGameObjectList();
-	void SetSelectedGameObject(Value_weak_ptr<GameObject> arg_obj) { m_selectedGameObject = arg_obj; }
+	void SetSelectedGameObject(Value_weak_ptr<GameObject> arg_obj);
 	std::string GetUniqueName(const std::string& arg_name);
 private:
 	void CreateViewGameObjectVector();
 	void CreateViewGameObjectVector_lock();
+	void SetSelectedGameObjectCtrl(Value_weak_ptr<GameObject> arg_obj);
+	void SetSelectedGameObjectShift(Value_weak_ptr<GameObject> arg_obj);
 	std::mutex m_mtx_addGameObject,m_mtx_update,m_mtx_viewObject,m_mtx_uniqueName;
 	List<Value_ptr<GameObject>> m_list_gameObjects,m_list_newGameObjects;
 	Value_ptr<List<Value_ptr<GameObject>>> m_vlp_list_viewGameObjects;
 	Value_weak_ptr<IScene> m_vwp_scene;
-	Value_weak_ptr<GameObject> m_selectedGameObject;
+	List<Value_weak_ptr<GameObject>> m_list_selectedGameObject;
 	std::string m_eventRegistKey;
 };
 
